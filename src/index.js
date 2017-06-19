b72c20bea1c99a469202df92f61bcc11f5817639
Bryan Clark <clarkbw@gmail.com>
2017-06-19 13:33:14 -0700

    Drop the default export

--- src/index.js
+++ src/index.js
@@ -4,6 +4,3 @@ import * as firefox from './firefox';
 export const match = chrome.isChrome ? chrome.match : firefox.match;
 export const url = chrome.isChrome ? chrome.url : firefox.url;
 export const dim = chrome.isChrome ? chrome.dim : firefox.url;
-
-// some backward compatability between 1.0 and 2.0
-export default match;
